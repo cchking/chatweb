3e56be2cdb490a97805747eab7f4f694c2bf74dd
trunicz <romelelicg_19@alu.uabcs.mx>
2023-05-29 12:47:49 -0700

    last update

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const app = express()
-const port = 5050
+const port = 5858
 const Gun = require('gun')
 
 app.use(Gun.serve)
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
 
 const server = app.listen(port, () => {
   console.log(`Gun server running on port ${port}🔥`)
