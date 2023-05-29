7833c278a24bbb5c24b864a780d73043a39fb474
trunicz <romelelicg_19@alu.uabcs.mx>
2023-05-29 09:33:33 -0700

    update all

--- server.js
+++ server.js
@@ -1,39 +1,12 @@
-const express = require('express');
-const http = require('http');
-const socketIO = require('socket.io');
+const express = require('express')
+const app = express()
+const port = 5050
+const Gun = require('gun')
 
-const app = express();
-const server = http.createServer(app);
-const io = socketIO(server);
-const path = require('path');
-const os = require('os');
+app.use(Gun.serve)
 
-// ...
+const server = app.listen(port, () => {
+  console.log(`Gun server running on port ${port}🔥`)
+})
 
-const networkInterfaces = os.networkInterfaces();
-const ipAddress = networkInterfaces['Ethernet'].find(interface => interface.family === 'IPv4').address;
-
-
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
-
-const connectedServers = new Set();
-io.on('connection', (socket) => {
-  console.log('Usuario conectado:', socket.id);
-
-  socket.on('disconnect', () => {
-    console.log('Usuario desconectado:', socket.id);
-  });
-
-  socket.on('message', (message) => {
-    console.log(`Mensaje recibido de ${socket.id}: ${message}`);
-
-    // Enviar el mensaje a todos los usuarios conectados
-    io.emit('message', { sender: socket.id, content: message });
-  });
-});
-
-server.listen(8000, () => {
-  console.log(`Servidor escuchando en http://${ipAddress}:8000`)
-});
+Gun({ web: server })
